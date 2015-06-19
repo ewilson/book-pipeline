b079c56e11a4cbb9708d1d970d9d347281fde3ba
Eric Wilson <e.wilson.1@elsevier.com>
2015-06-18 21:48:01 -0400

    identifying minor sections

--- baxter/sections.py
+++ baxter/sections.py
@@ -7,9 +7,23 @@ MAJOR_SECTION_PATTERN = re.compile(r"""^[A-Z]     # First character capital lett
                                        [A-Z]+$    # ends with capital letter""", re.VERBOSE)
 
 
+MINOR_SECTION_PATTERN = re.compile(r"""^[A-Z]                      # First character capital letter
+                                       +[^a-z]+\n                  # no lowercase letters on line
+                                       [ \t]*\n                    # next line is whitespace only
+                                       [ \t]*[A-Z]+[a-z ][a-z]+    # third line has lowercase letters
+                                       """, re.VERBOSE)
+
 def extract_headers(chunks):
     return []
 
 
 def is_major_section(chunk):
     return MAJOR_SECTION_PATTERN.match(chunk) is not None
+
+
+def clean_major_section(s):
+    return ' '.join(re.split(r'\n[ \t]*\n[ \t]*', s))
+
+
+def is_minor_section(chunk):
+    return MINOR_SECTION_PATTERN.match(chunk) is not None
--- baxter/test_sections.py
+++ baxter/test_sections.py
@@ -23,3 +23,34 @@ def test_if_no_blank_line_is_not_major_section():
                  THAT I WROTE"""
 
     assert not is_major_section(passage)
+
+
+def test_clean_major_section():
+    passage = """CHAPTER 1
+
+                 THE OVERSIGHT OF OURSELVES"""
+
+    assert clean_major_section(passage) == "CHAPTER 1 THE OVERSIGHT OF OURSELVES"
+
+
+def test_is_minor_section():
+    passage = """SECTION 1 -- THE NATURE OF THIS OVERSIGHT
+
+   Having showed you, What it is to take heed to ourselves, I am to show
+   you, next to all the flock in proportion to the measure
+   of their sin."""
+
+    assert not is_major_section(passage)
+    assert is_minor_section(passage)
+
+
+def test_is_not_minor_section():
+    passage = """SECTION 1 -- THE NATURE OF THIS OVERSIGHT
+   Having showed you, What it is to take heed to ourselves, I am to show
+   you, next to all the flock in proportion to the measure
+   of their sin."""
+
+    assert not is_major_section(passage)
+    assert not is_minor_section(passage)
+
+
